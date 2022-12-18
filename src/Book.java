09ad506828a79d0b17748d88f89221652e567926
yuliavolkova <uvolkova486@gmail.com>
2022-12-19 00:49:10 +0900

    Сделано ДЗ

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private final String bookTitle;
+    private final Author author;
+    private int yearOfPublication;
+
+    public Book(String bookTitle, Author author, int yearOfPublication) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
